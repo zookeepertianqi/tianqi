e46fa87b6127c0eb935935d571456021e417a939
qibaofu <qibaofu@qi.lan>
2019-05-02 09:16:18 +0800

    提交初始代码

---
+++ src/main/java/com/tianqi/thread/TianqiThread/App.java
@@ -0,0 +1,13 @@
+package com.tianqi.thread.TianqiThread;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
